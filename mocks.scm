92ccd6e377261b48c733238ae71e16d43bad4ab0
Marc-André Bélanger <marc-andre.belanger@asciimail.com>
2021-02-10 17:06:02 -0500

    Mocks now work.

--- mocks.scm
+++ mocks.scm
@@ -7,38 +7,47 @@
 (import (github.com/feeley pyffi))
 
 \import math
-(define pi (\math.pi))
-(define py-sqrt \math.sqrt)
-(define three (py-sqrt 9))
-(define (area r)
+(define pi \math.pi)
+(define (py-sqrt n) \math.sqrt(\n))
+(define res (py-sqrt 9))
+(for-each display `("Square root of 9: " ,res "\n"))
+
+(define (circle-area r)
   (* pi r r))
-(define (comb n k)
-  \math.comb(\n, \k))
-(define comb* \math.comb)
+(for-each display `("Area of circle of radius pi: "
+                    ,(circle-area pi) "\n"))
+(define (hypot x y)
+  \math.hypot(\x, \y))
+(for-each display `("Hypothenuse of triangle of sides of length 3 and 4: "
+                    ,(hypot 3 4) "\n"))
 
 \import time
-(define time \time.time)
-(define sleep \time.sleep)
-(define then (time))
-(sleep 5)
-(define now (time))
+(define (_time) \time.time())
+(define (sleep n) \time.sleep(\n))
+(define then (_time))
+(for-each display `("Current time is: " ,then "\n"))
+(display "Sleeping for 3 seconds...") (newline)
+(sleep 3)
+(define now (_time))
+(for-each display `("Current time is: " ,now "\n"))
 (define delta (- now then))
+(for-each display `("Time delta is : " ,delta "\n"))
 
-\from datetime import datetime
-(define then \datetime.now())
-(sleep 10)
-(define now \datetime.now())
-(define delta (- now then))
-(define delta* \\now - \then)
-(define seconds \\(delta).total_seconds())
+;; \import datetime
+;; (define then \datetime.datetime.now())
+;; (sleep 3)
+;; (define now \datetime.datetime.now())
+;; (define delta (- now then))
+;; (define seconds \\(delta).total_seconds())
 
-\import requests
-(define url "https://jsonplaceholder.typicode.com/todos/1")
-(define r \requests.get(\url))
-(define r.json \\(r).json())
+;; (pip install requests)
+;; \import requests
+;; (define url "https://jsonplaceholder.typicode.com/todos/1")
+;; (define r \requests.get(\url))
+;; (define r.json \\(r).json())
 
-\from flask import Flask
-(define app \Flask(__name__))
-(@ app route (list '/')
-       (define (hello-world)
-         "Hello, world!"))
+;; \from flask import Flask
+;; (define app \Flask(__name__))
+;; (@ app route (list '/')
+;;        (define (hello-world)
+;;          "Hello, world!"))
